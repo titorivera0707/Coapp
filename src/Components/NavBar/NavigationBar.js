7ee0c0841d06dee245e5e32182000b588ced2082
Tito Rivera <titorivera0707@gmail.com>
2021-03-31 21:56:38 -0700

    Got the modal to begin working now we can move onto actually adding the questions to the modals and saving the information.

--- src/Components/NavBar/NavigationBar.js
+++ src/Components/NavBar/NavigationBar.js
@@ -1,22 +1,25 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import './NavigationBar.css'
+import Modall from "../Modal/Modal"
 
 function NavigationBar() {
+
+    const [modalShow, setModalShow] = React.useState(false);
 
     // Onclick event handler for the login portion of the application.
     function handleLogin(e) {
         e.preventDefault();
         console.log("Login page will appear.")
+        setModalShow(true)
     }
 
     // Onclick event handler for the registration portion of the application.
     function handleRegister(e) {
         e.preventDefault();
         console.log("Registration page will appear.")
+        setModalShow(true)
     }
-
-    
 
     return(
         <div>
@@ -29,14 +32,16 @@ function NavigationBar() {
                 </Nav>
                 <div>
 
-            {/* Simple Navbar that will most likely be changed later on but for now just to give us an idea of what will be there. */}
-            <Navbar />
-
             <button className="btnShp" onClick={handleLogin}>Login</button>
             <button className="btnShp" onClick={handleRegister}>Register</button>
 
         </div>
             </Navbar>
+
+            <Modall
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
         </div>
     )
 }
