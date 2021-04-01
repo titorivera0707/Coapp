34abb56241191cba5f0faddda2dd0c54f574fb29
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-01 14:01:28 -0700

    done styling header-navBar for today

--- src/Components/NavBar/Header.js
+++ src/Components/NavBar/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Nav } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 import './Header.css'
 import ModalComp from "../Modal/ModalComp"
 import logo from '../../Assets/logo_transp.png';
@@ -10,21 +10,20 @@ function Header() {
 
     return(
         <div className="ModalComp__header">
-         <img src= {logo} alt="logo" href="/"/>
+        <Navbar expand="lg">
+         <Nav.Link href='/'><img src ={logo} alt="logo" style={{"objectFit": "contain", "height":"6rem"}}/></Nav.Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" variant="outline-secondary"/>
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
         <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/matchMe">Match Me</Nav.Link>
         <Nav.Link href="/explore">Explore</Nav.Link>
         </Nav>
-            <div>
+        <div className="ModalComp">
+        <ModalComp show={modalShow} onHide={() => setModalShow(false)}/>
         </div>
-        <ModalComp
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-
-
-   
+       </Navbar.Collapse>   
+        </Navbar>
         </div>
     )
 }
