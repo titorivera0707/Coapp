d88a47f09e1f49644ca0ad9ae92e75390412c1a5
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-01 10:27:39 -0700

    Login and register done

--- src/Components/Modal/ModalComp.js
+++ src/Components/Modal/ModalComp.js
@@ -1,31 +1,150 @@
-import React from "react"
-import './Modal.css'
-import { Modal, Button } from "react-bootstrap"
+import React, { useState, useEffect } from 'react'
+import './Modal.css';
+import Modal from '@material-ui/core/Modal';
+import logo from '../../Assets/logo_transp.png';
+import { makeStyles } from '@material-ui/core/styles';
+import { auth } from '../../Firebase';
+import { Button, Input } from '@material-ui/core';
 
-function ModalComp(props) {
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: ` translate(-${top}%, -${left}%)`,
+  }
+}
+const useStyles = makeStyles((theme) =>({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+function ModalComp() {
+
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle)
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((autUser) => {
+    if (autUser) {
+      console.log(autUser);
+      setUser(autUser);
+    } else {
+      setUser(null);
+    }
+
+    }) 
+    return() =>{
+      unsubscribe();
+    }
+    } , [user, username]);
+
+    const signUp =(event) => {
+      event.preventDefault();
+
+      auth.createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+       return authUser.user.updateProfile({
+          displayName:  username
+        })
+      })
+      .catch((error) => alert(error.message));
+      setOpen(false);
+    }
+
+    const signIn =(event) => {
+      event.preventDefault();
+      auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+      setOpenSignIn(false);
+    }
+
+
 
     return(
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Register
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+      <div className="ModalComp">
+       <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <form className="ModalComp__register">
+        
+       <center>
+      <img className="ModalComp__headerImage" src={logo} alt="ModalComp logo" />
+      </center>
+      <Input
+      placeholder="username"
+      type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)} />
+      <Input 
+      placeholder="email"
+      type="text"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)} />
+      <Input 
+      placeholder="password"
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)} />
+      <Button type="submit" onClick={signUp}>Sign up</Button>
+      </form>
+       
+    </div>
+      </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <form className="ModalComp__register">
+       <center>
+      <img className="ModalComp__headerImage" src={logo} alt="ModalComp logo" />
+      </center>
+   
+      <Input 
+      placeholder="email"
+      type="text"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)} />
+      <Input 
+      placeholder="password"
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)} />
+      <Button type="submit" onClick={signIn}>Log In</Button>
+      </form>
+       
+    </div>
+      </Modal>
+      <div className="ModalComp__header">
+    
+        {user ? (
+        <Button onClick={() => auth.signOut()}>Log out</Button>
+      ): (
+        <div className="ModalComp__loginContainer">
+        <Button onClick={() => setOpenSignIn(true)}>Login</Button>
+        <Button onClick={() => setOpen(true)}>Register</Button>
+        </div>
+      )}
+      </div>
+    </div>
     )
 }
 
--- src/Components/NavBar/Header.js
+++ src/Components/NavBar/Header.js
@@ -1,46 +1,31 @@
 import React from "react"
-import { Navbar, Nav } from "react-bootstrap"
+import { Nav } from "react-bootstrap"
 import './Header.css'
 import ModalComp from "../Modal/ModalComp"
+import logo from '../../Assets/logo_transp.png';
 
 function Header() {
 
     const [modalShow, setModalShow] = React.useState(false);
 
-    // Onclick event handler for the login portion of the application.
-    function handleLogin(e) {
-        e.preventDefault();
-        console.log("Login page will appear.")
-        setModalShow(true)
-    }
-
-    // Onclick event handler for the registration portion of the application.
-    function handleRegister(e) {
-        e.preventDefault();
-        console.log("Registration page will appear.")
-        setModalShow(true)
-    }
-
     return(
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">Pup Match</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/matchMe">Match Me</Nav.Link>
-                    <Nav.Link href="/explore">Explore</Nav.Link>
-                </Nav>
-                <div>
-
-            <button className="btnShp" onClick={handleLogin}>Login</button>
-            <button className="btnShp" onClick={handleRegister}>Register</button>
+        <div className="ModalComp__header">
+      
+         <img src= {logo} alt="logo"/>
+        <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/matchMe">Match Me</Nav.Link>
+        <Nav.Link href="/explore">Explore</Nav.Link>
+        </Nav>
+            <div>
         </div>
-            </Navbar>
-
-            <ModalComp
+        <ModalComp
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
+
+
+   
         </div>
     )
 }
